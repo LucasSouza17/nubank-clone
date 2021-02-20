18a37e50dd2ab33913c7c88ebe3eb65a4492912d
Lucas Fernando de Souza <62787018+LucasSouza17@users.noreply.github.com>
2021-02-20 02:28:25 -0300

    Ident

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import Feather from '@expo/vector-icons/build/Feather'
 import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
 import { styles } from './styles';
 
 type HeaderProps = {
-    name: String
+  name: String
 }
 
 const Header = ({ name }: HeaderProps) => {
-    return (
-        <View style={styles.header}>
-            <Text style={styles.name}>Olá, {name}</Text>
-            <View style={styles.containerIcons}>
-                <View style={styles.icon}>
-                    <Feather name="eye" size={wp('5.5%')} color="white" />
-                </View>
-                <View style={styles.icon}>
-                    <Feather name="settings" size={wp('5.5%')} color="white" />
-                </View>
-            </View>
-        </View>
-    )
+  return (
+    <View style={styles.header}>
+      <Text style={styles.name}>Olá, {name}</Text>
+      <View style={styles.containerIcons}>
+        <TouchableOpacity style={styles.icon}>
+          <Feather name="eye" size={wp('5.5%')} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.icon}>
+          <Feather name="settings" size={wp('5.5%')} color="white" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
 }
 
 export default Header;
